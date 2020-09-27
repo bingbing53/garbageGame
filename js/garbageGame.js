1bf0027a0d10356729e4e4d4178e71efb79f58ea
songbing0503 <284600750@qq.com>
2020-09-27 17:10:32 +0800

    游戏静态页面

---
+++ js/garbageGame.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        goGamePage() {
+            window.open('gamePage.html');
+        }
+    },
+    watch: {},
+    mounted() {}
+})
