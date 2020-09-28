388c66c730111752f6e2302fc2eb47204ac3beeb
songbing0503 <284600750@qq.com>
2020-09-28 15:06:26 +0800

    动画

--- js/garbageGame.js
+++ js/garbageGame.js
@@ -2,14 +2,72 @@ new Vue({
     el: '#app',
     data() {
         return {
-
+            garbageImgList: [{
+                    label: 1,
+                    url: 'img/垃圾_slices/图层 1.png'
+                },
+                {
+                    label: 2,
+                    url: 'img/垃圾_slices/图层 2.png'
+                },
+                {
+                    label: 3,
+                    url: 'img/垃圾_slices/图层 3.png'
+                },
+                {
+                    label: 4,
+                    url: 'img/垃圾_slices/图层 4.png'
+                },
+                {
+                    label: 2,
+                    url: 'img/垃圾_slices/图层 5.png'
+                },
+                {
+                    label: 1,
+                    url: 'img/垃圾_slices/图层 6.png'
+                },
+                {
+                    label: 2,
+                    url: 'img/垃圾_slices/图层 7.png'
+                },
+                {
+                    label: 3,
+                    url: 'img/垃圾_slices/图层 8.png'
+                },
+                {
+                    label: 4,
+                    url: 'img/垃圾_slices/图层 8.png'
+                },
+                {
+                    label: 3,
+                    url: 'img/垃圾_slices/图层 10.png'
+                }
+            ],
+            // label: this.garbageImgList[0].label
         }
     },
     methods: {
         goGamePage() {
             window.open('gamePage.html');
+        },
+        goGarbage(id) {
+            let label = id.currentTarget.id
+            console.log(label)
+            let msg = this.garbageImgList[0].label
+            if (msg == label) {
+                console.log("恭喜你答对了")
+            } else {
+                console.log("很遗憾，打错了")
+            }
+            // this.garbageImgList.splice(0, 1);
+            // console.log(this.garbageImgList)
+
         }
     },
-    watch: {},
-    mounted() {}
+    watch: {
+        // label
+    },
+    mounted() {
+        console.log(this.garbageImgList[0].url)
+    }
 })
